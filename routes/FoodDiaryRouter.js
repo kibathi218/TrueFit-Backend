75468d102d749a2fdcf2baf3dd4622651aae5019
kibathi218 <kibathi2180@gmail.com>
2022-05-16 00:10:11 -0400

    backend auth is fixed

--- routes/FoodDiaryRouter.js
+++ routes/FoodDiaryRouter.js
@@ -1,21 +1,29 @@
 const Router = require('express').Router()
 const controller = require('../controllers/FoodDiaryController')
-// const middleware = require('../middleware')
+const middleware = require('../middleware')
 
 Router.get(
     '/:user_id',
-    controller.GetFoodDiary
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.GetFoodDiary,
 )
 Router.post(
     '/:user_id',
+    middleware.stripToken,
+    middleware.verifyToken,
     controller.CreateFoodDiary
 )
 Router.put(
     '/:food_id',
+    middleware.stripToken,
+    middleware.verifyToken,
     controller.UpdateFoodDiary
 )
 Router.delete(
     '/:food_id',
+    middleware.stripToken,
+    middleware.verifyToken,
     controller.DeleteFood
 )
 
